9ceac568210c4ad340c13b52e069d40fae90cad6
UktamovAkbar <slavok2612@gmail.com>
2023-05-16 20:45:38 +0500

    added with shablon

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./footer/Footer";
 import Home from "./home/Home";
 import Navbar from "./navbar/Navbar";
-import About from "./about/About"
+import About from "./about/About";
 import Books from "./books/Books";
+import Reklama from "./reklama/Reklama";
+import SignIn from "./sign-in/SignIn";
+import SignUp from "./sign-up/SignUp";
+import NotFoundPage from "./not-found-page/NotFoundPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFoundPage />} />
       <Route path="/navbar" element={<Navbar />} />
-      <Route path="/footer" element={<Footer/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/books" element={<Books/>} />
-
+      <Route path="/footer" element={<Footer />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/books" element={<Books />} />
+      <Route path="/reklama" element={<Reklama />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 };
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 import './Home.css'
+import Books from "../books/Books";
+import Reklama from "../reklama/Reklama";
 
 const Home = () => {
   return (
@@ -9,7 +11,30 @@ const Home = () => {
       <div className="">
         <Navbar />
       </div>
-   
+        <div className="">
+          <div className="home_section">
+            <div className="home_columns">
+              <div className="home_column is-20"></div>
+              <div className="home_column is-30">
+                <h1>Library - читайте книги <br /> бесплатно</h1>
+                <button className="home_regist_btn">Зарегистрироваться</button>
+              </div>
+              <div className="home_column is-50">
+                <img src="./image/devushka-kniga.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <Books/>
+        </div>
+        <div className="">
+          <Books/>
+        </div>
+        <div className="">
+          <Books/>
+        </div>
+        <div className=""><Reklama/></div>
       <div className="home_footer">
         <Footer />
       </div>
