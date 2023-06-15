4a6da932a4c7db003471834fbc159fdac39fc5fe
Amir <shodiyevamir999@gmail.com>
2023-06-15 22:23:51 +0500

    added

--- src/sign-up/SignUp.jsx
+++ src/sign-up/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaTwitter, FaInstagram, FaTelegram, FaPinterest, FaGoogle } from 'react-icons/fa'
+import { FaTwitter, FaInstagram, FaTelegram, FaPinterest, FaWhatsapp} from 'react-icons/fa'
 import "./SignUp.css"
 
 const SignUp = () => {
@@ -10,10 +10,13 @@ const SignUp = () => {
       <div className='signup_box'>
         <h1 className='signup_box_h1'>Вход</h1>
         <Link className='x' to='/'>x</Link>
+        <p className='signup_box_p1'>Имя:</p>
+        <input className='signup_box_inp1' type="text" />
         <p className='signup_box_p1'>Почта или телефон:</p>
         <input className='signup_box_inp1' type="text" name="" id="" />
         <p className='signup_box_p1'>Пароль:</p>
         <input className='signup_box_inp1' type="password" />
+   
         <button className='signup_box_btn'><h1>Войти</h1></button>
         <div className="signup_bottom">
           <Link to='/'><button className="signup_bottom1"><h1 className='signup_bottom1_h1'><FaInstagram></FaInstagram></h1></button></Link>
@@ -21,10 +24,11 @@ const SignUp = () => {
         </div>
 
         <div className="signup_center">
-          <Link><button className='signup_bottom3'><h1><FaTelegram/></h1></button></Link>
-          <Link><button className='signup_bottom4'><h1> <FaPinterest/> </h1></button></Link>
-          <Link><button className='signup_bottom5'><h1> <FaGoogle/> </h1></button></Link>
+          <Link to='/'><button className='signup_bottom3'><h1> <FaTelegram/></h1></button></Link>
+          <Link to='/'><button className='signup_bottom4'><h1> <FaWhatsapp/> </h1></button></Link>
+          <Link to='/'><button className='signup_bottom5'><h1> <FaPinterest/> </h1></button></Link>
         </div>
+
 
       </div>
     </div>
