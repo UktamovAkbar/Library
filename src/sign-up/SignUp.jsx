e3b80061f3b57c1eeaa828fca4deeb083f89678d
Amir <shodiyevamir999@gmail.com>
2023-06-15 22:59:27 +0500

    added

--- src/sign-up/SignUp.jsx
+++ src/sign-up/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaTwitter, FaInstagram, FaTelegram, FaPinterest, FaWhatsapp} from 'react-icons/fa'
+import { FaTwitter, FaInstagram, FaTelegram, FaPinterest, FaWhatsapp, FaFacebook, FaYoutube } from 'react-icons/fa'
 import "./SignUp.css"
 
 const SignUp = () => {
@@ -16,7 +16,7 @@ const SignUp = () => {
         <input className='signup_box_inp1' type="text" name="" id="" />
         <p className='signup_box_p1'>Пароль:</p>
         <input className='signup_box_inp1' type="password" />
-   
+
         <button className='signup_box_btn'><h1>Войти</h1></button>
         <div className="signup_bottom">
           <Link to='/'><button className="signup_bottom1"><h1 className='signup_bottom1_h1'><FaInstagram></FaInstagram></h1></button></Link>
@@ -24,11 +24,15 @@ const SignUp = () => {
         </div>
 
         <div className="signup_center">
-          <Link to='/'><button className='signup_bottom3'><h1> <FaTelegram/></h1></button></Link>
-          <Link to='/'><button className='signup_bottom4'><h1> <FaWhatsapp/> </h1></button></Link>
-          <Link to='/'><button className='signup_bottom5'><h1> <FaPinterest/> </h1></button></Link>
+          <Link to='/'><button className='signup_bottom3'><h1> <FaTelegram /></h1></button></Link>
+          <Link to='/'><button className='signup_bottom4'><h1> <FaWhatsapp /> </h1></button></Link>
+          <Link to='/'><button className='signup_bottom5'><h1> <FaPinterest /> </h1></button></Link>
         </div>
 
+        <div className="signup_bottom">
+          <Link to='/'><button className="signup_bottom11"><h1 className='signup_bottom1_h1'><FaYoutube/> </h1></button></Link>
+          <Link to='/'> <button className="signup_bottom22"><h1 className='signup_bottom1_h2'><FaFacebook /></h1></button></Link>
+        </div>
 
       </div>
     </div>
